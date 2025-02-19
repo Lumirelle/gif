d84d3a9bcae440e18fcc32aaaa6ea7771d8b72a4
Lyana <shabbyacc@outlook.com>
2025-02-19 18:24:42 +0800

    style: code style

--- Modules/Core/Core.psm1
+++ Modules/Core/Core.psm1
@@ -9,14 +9,14 @@ Config name
 Config value (hashtable)
 #>
 function Initialize-GifCore {
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$Config,
-        [Parameter(Mandatory = $true)]
-        [hashtable]$Value
-    )
-    Set-Variable $Config $Value -Scope Script # Share with Core.psm1
-    Write-GifOutput "[Core.psm1::Initialize-GifCore::Config=Value]`n$Config = $($Value | ConvertTo-Json)" -Level verbose
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]$Config,
+    [Parameter(Mandatory = $true)]
+    [hashtable]$Value
+  )
+  Set-Variable $Config $Value -Scope Script # Share with Core.psm1
+  Write-GifOutput "[Core.psm1::Initialize-GifCore::Config=Value]`n$Config = $($Value | ConvertTo-Json)" -Level verbose
 }
 
 <#
@@ -26,14 +26,14 @@ Reset the workspace (such as reset branch back to the source branch)
 Reset config
 #>
 function Reset-GifWorkspace {
-    param (
-        [Parameter(Mandatory = $true)]
-        [hashtable]$ResetConfig
-    )
-    Write-GifOutput 'Reset workspace...' -ShowCurrentBranch
-    if ($ResetConfig['Branch']) {
-        GifCore switch $ResetConfig['Branch']
-    }
+  param (
+    [Parameter(Mandatory = $true)]
+    [hashtable]$ResetConfig
+  )
+  Write-GifOutput 'Reset workspace...' -ShowCurrentBranch
+  if ($ResetConfig['Branch']) {
+    GifCore switch $ResetConfig['Branch']
+  }
 }
 
 <#
@@ -41,24 +41,24 @@ function Reset-GifWorkspace {
 GifCore, execute git command
 #>
 function GifCore {
-    begin {
-        $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-        Write-GifOutput "[Core.psm1::GifCore::Exec] git $($args -join ' ')" -Level Verbose
+  begin {
+    $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    Write-GifOutput "[Core.psm1::GifCore::Exec] git $($args -join ' ')" -Level Verbose
+  }
+  process {
+    try {
+      $Command = $args -join ' '
+      if ($Command -match '[;&|]') {
+        throw 'Dangerous characters detected in command!'
+      }
+      $Output = & git @args 2>&1 | Out-String
+      if (-not $?) {
+        throw $Output
+      }
+      return $Output
     }
-    process {
-        try {
-            $Command = $args -join ' '
-            if ($Command -match '[;&|]') {
-                throw 'Dangerous characters detected in command!'
-            }
-            $Output = & git @args 2>&1 | Out-String
-            if (-not $?) {
-                throw $Output
-            }
-            return $Output
-        }
-        finally {
-            Write-GifOutput "[Core.psm1::GifCore::TimeCost] $($Stopwatch.Elapsed.TotalSeconds.ToString('0.00s'))" -Level Verbose
-        }
+    finally {
+      Write-GifOutput "[Core.psm1::GifCore::TimeCost] $($Stopwatch.Elapsed.TotalSeconds.ToString('0.00s'))" -Level Verbose
     }
+  }
 }
